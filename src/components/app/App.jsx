201d07dfce7f2aee2edf6cce0dd8225fa4d4f488
Justin W Soto <soto@pdx.edu>
2021-11-01 20:58:57 -0700

    now its ok

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RandomColor from '../../containers/RandomColor'
+import '../app/main.css';
 
 export default function App() {
 
--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -6,7 +6,7 @@ class RandomColor extends Component {
     };
 
     handleColor() {
-        const colorList = ['#6C9A8B', '#E8998D', '#EED2CC', '#FBF7F4', '#A1683A', '#5B8677'];
+        const colorList = ['#6C9A8B', '#E8998D', '#EED2CC', '#FBF7F4', '#A1683A', '#5B8677', '#94E8B4', '#72BDA3', '#5E8C61', '#4E6151', '#FFEDE1', '#F9FBF2', '#D7F9FF', '#AFCBFF', '#0E1C36' ];
         const randomNum = Math.ceil(Math.random() * colorList.length);
       
 
